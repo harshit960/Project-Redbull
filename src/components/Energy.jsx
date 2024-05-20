1d5a1b6ebda3a1e510f92050af1ddd893756240b
harshit960 <raj.harshit960@gmail.com>
2024-05-20 17:18:56 +0530

    push

--- src/components/Energy.jsx
+++ src/components/Energy.jsx
@@ -20,80 +20,81 @@ function Black() {
     <>
       <div className="w-full bg-[#333] relative">
         <div className="top-layer absolute top-[-12%] sm:top-[-25px]">
-        <div className="top-layer absolute top-[-6%] sm:top-[-36px]">
-          <img src="black-border.svg" alt="" />
-        </div>
+          <div className="top-layer absolute top-[-6%] sm:top-[-36px]">
+            <img src="black-border.svg" alt="" />
+          </div>
 
-        <div className="relative">
-          <img className="boom-layer" src="boom_layer.svg" alt="" />
-          <img
-            className="top-layer z-30 absolute top-0"
-            src="top_layer.svg"
-            alt=""
-          />
-        </div>
-
-        <div className="absolute z-50 bg-[#333] font-['bebas_neue'] font-bold text-[#ffedd7] leading-[28vmax] text-[30vmax] md:text-[15vmax] md:leading-none px-4 py-12">
-          <h1>GIVES YOU</h1>
-          <h1 className="flex items-center gap-6 overflow-hidden">
-            WIIINGS <GiSpikyWing />
-          </h1>
-        </div>
-
-        <div className="flex md:flex-col md:gap-8 justify-between px-10 pb-12 text-white">
-          <span className="flex flex-col md:items-center gap-4">
-            <FaLeaf size={50} color="#ffedd7" className="opacity-50" />
-            <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
-              Proudly made with <br /> natural ingredients
-            </h1>
-          </span>
-          <hr className="hidden md:flex" />
-          <span className="flex flex-col md:items-center gap-4">
-            <IoBatteryCharging
-              size={50}
-              color="#ffedd7"
-              className="opacity-50"
+          <div className="relative">
+            <img className="boom-layer" src="boom_layer.svg" alt="" />
+            <img
+              className="top-layer z-30 absolute top-0"
+              src="top_layer.svg"
+              alt=""
             />
-            <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
-              Keeps you mentally <br /> charged and ready
-            </h1>
-          </span>
-          <hr className="hidden md:flex" />
-          <span className="flex flex-col md:items-center gap-4">
-            <GiDiamondsSmile size={50} color="#ffedd7" className="opacity-50" />
-            <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
-              Your cheerful pick
-              <br />
-              -me-up
-            </h1>
-          </span>
-        </div>
+          </div>
 
-        <div className="drinks px-6 left-0 z-[99] flex gap-6 overflow-x-hidden">
-          <img
-            src="redbull-y.png"
-            className="w-[450px] md:w-[200px] rounded-xl"
-            alt=""
-          />
-          <img
-            src="redbull-r.png"
-            className="w-[450px] md:w-[200px] rounded-xl"
-            alt=""
-          />
-          <img
-            src="redbull-s.png"
-            className="w-[450px] md:w-[200px] rounded-xl"
-            alt=""
-          />
-          <img
-            src="redbull-b.png"
-            className="w-[450px] md:w-[200px] rounded-xl"
-            alt=""
-          />
+          <div className="absolute z-50 bg-[#333] font-['bebas_neue'] font-bold text-[#ffedd7] leading-[28vmax] text-[30vmax] md:text-[15vmax] md:leading-none px-4 py-12">
+            <h1>GIVES YOU</h1>
+            <h1 className="flex items-center gap-6 overflow-hidden">
+              WIIINGS <GiSpikyWing />
+            </h1>
+          </div>
+
+          <div className="flex md:flex-col md:gap-8 justify-between px-10 pb-12 text-white">
+            <span className="flex flex-col md:items-center gap-4">
+              <FaLeaf size={50} color="#ffedd7" className="opacity-50" />
+              <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
+                Proudly made with <br /> natural ingredients
+              </h1>
+            </span>
+            <hr className="hidden md:flex" />
+            <span className="flex flex-col md:items-center gap-4">
+              <IoBatteryCharging
+                size={50}
+                color="#ffedd7"
+                className="opacity-50"
+              />
+              <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
+                Keeps you mentally <br /> charged and ready
+              </h1>
+            </span>
+            <hr className="hidden md:flex" />
+            <span className="flex flex-col md:items-center gap-4">
+              <GiDiamondsSmile size={50} color="#ffedd7" className="opacity-50" />
+              <h1 className="font-[gilroy] text-4xl md:text-center opacity-50 font-medium leading-none">
+                Your cheerful pick
+                <br />
+                -me-up
+              </h1>
+            </span>
+          </div>
+
+          <div className="drinks px-6 left-0 z-[99] flex gap-6 overflow-x-hidden">
+            <img
+              src="redbull-y.png"
+              className="w-[450px] md:w-[200px] rounded-xl"
+              alt=""
+            />
+            <img
+              src="redbull-r.png"
+              className="w-[450px] md:w-[200px] rounded-xl"
+              alt=""
+            />
+            <img
+              src="redbull-s.png"
+              className="w-[450px] md:w-[200px] rounded-xl"
+              alt=""
+            />
+            <img
+              src="redbull-b.png"
+              className="w-[450px] md:w-[200px] rounded-xl"
+              alt=""
+            />
+          </div>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+      );
 }
 
-export default Black;
+      export default Black;
